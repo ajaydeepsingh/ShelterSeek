4c35fafb82d24476049ad92d9a74b95a215785b4
Ajay Singh <singhajay08@gmail.com>
2018-02-13 15:16:21 -0500

    updated gitignore
    -

---
+++ app/src/main/java/com/example/jmc19/shelterseeker/USERRegistrationScreen.java
@@ -0,0 +1,13 @@
+package com.example.jmc19.shelterseeker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class USERRegistrationScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userregistration_screen);
+    }
+}
