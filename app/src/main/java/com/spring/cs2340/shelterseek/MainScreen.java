ecf07dadd34784ecd4c71479e68f79b2cf9a00d4
Ajay Singh <singhajay08@gmail.com>
2018-02-16 12:04:10 -0500

    Add files via upload

---
+++ app/src/main/java/com/spring/cs2340/shelterseek/MainScreen.java
@@ -0,0 +1,26 @@
+package com.spring.cs2340.shelterseek.controller;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.spring.cs2340.shelterseek.R;
+
+public class MainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        Button logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent newIntent = new Intent(getBaseContext(), WelcomeScreen.class);
+                startActivity(newIntent);
+            }
+        });
+    }
+}
