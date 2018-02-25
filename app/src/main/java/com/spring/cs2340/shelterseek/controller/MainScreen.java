3ead2bda9810718a2abccefcda0ca688f0f3dcb8
Cozzone, Justin Michael <jcozzone3@gatech.edu>
2018-02-25 12:44:16 -0500

    Added a recyclerview
    Help with M6

--- app/src/main/java/com/spring/cs2340/shelterseek/controller/MainScreen.java
+++ app/src/main/java/com/spring/cs2340/shelterseek/controller/MainScreen.java
@@ -14,13 +14,6 @@ public class MainScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
-        Button logoutButton = (Button) findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent newIntent = new Intent(getBaseContext(), WelcomeScreen.class);
-                startActivity(newIntent);
-            }
-        });
+        
     }
 }
