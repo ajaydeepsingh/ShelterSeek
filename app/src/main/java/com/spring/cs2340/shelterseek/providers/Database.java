cd7d3701d81b3ba8a2b1c8f4731c4b03be0ee479
Ajay Singh <singhajay08@gmail.com>
2018-03-27 19:43:56 -0400

    Started Database try
    -

---
+++ app/src/main/java/com/spring/cs2340/shelterseek/providers/Database.java
@@ -0,0 +1,15 @@
+package com.spring.cs2340.shelterseek.providers;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.sql.SQLException;
+
+
+/**
+ * Created by ajay on 3/27/18.
+ */
+
+public class Database {
+}
